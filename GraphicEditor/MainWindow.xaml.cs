7ad8d92f66e7e97edb9547823aa79ff82d9915e6
NemanjaTam <nemanja.tam69@gmail.com>
2020-10-30 18:23:56 +0100

    #Updated hospital map and added some animations

--- GraphicEditor/MainWindow.xaml.cs
+++ GraphicEditor/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,51 @@ namespace GraphicEditor
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            MainFrame.Content = new HospitalMap(this);
+
+        }
+
+        private void ShowHospitalMap(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Content = new HospitalMap(this);
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            storyboard.Children.Add(doubleAnimation);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(Canvas.OpacityProperty));
+            Storyboard.SetTargetName(doubleAnimation, MainFrame.Name);
+            storyboard.Begin(this);
+        }
+
+        private void ShowBuilding1Floor(object sender, MouseButtonEventArgs e)
+        {
+            //MainFrame.Content = new Building1FloorPlan(this);
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            storyboard.Children.Add(doubleAnimation);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(Canvas.OpacityProperty));
+            Storyboard.SetTargetName(doubleAnimation, MainFrame.Name);
+            storyboard.Begin(this);
+        }
+
+        private void ShowBuilding2Floor(object sender, MouseButtonEventArgs e)
+        {
+            Storyboard storyboard = new Storyboard();
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            storyboard.Children.Add(doubleAnimation);
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(Canvas.OpacityProperty));
+            Storyboard.SetTargetName(doubleAnimation, MainFrame.Name);
+            storyboard.Begin(this);
+            //MainFrame.Content = new Building2FloorPlan(this);
         }
     }
 }
