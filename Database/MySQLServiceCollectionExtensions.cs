547c0178d828707d84381975665384b806131ff3
Bojan <vujic.ra170.2017@uns.ac.rs>
2020-10-31 14:27:45 +0100

    Initialized database

---
+++ Database/MySQLServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+
+namespace Database
+{
+    public static class MySQLServiceCollectionExtensions
+    {
+        
+        public static IServiceCollection RegisterMySQLDataServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<MySQLContext>(options =>
+                options.UseMySql(configuration.GetConnectionString("MySQLConnectionString")));
+
+            return services;
+        }
+    }
+}
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.RegisterMySQLDataServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
