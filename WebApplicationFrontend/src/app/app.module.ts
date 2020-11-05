628ef41b966a48c24bd8e6ff2c9d0cda46930240
Bojan <vujic.ra170.2017@uns.ac.rs>
2020-11-05 16:39:09 +0100

    Minor fixes

--- WebApplicationFrontend/src/app/app.module.ts
+++ WebApplicationFrontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeedbackComponent } from './feedback/feedback.component';
+
 
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,8 +13,8 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FeedbackComponent
+    AppComponent
+   
   ],
   imports: [
     BrowserModule,
