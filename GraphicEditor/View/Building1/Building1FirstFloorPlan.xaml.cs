dfeec90482370f6b229df982b2b0b30ba95bfdea
NemanjaTam <nemanja.tam69@gmail.com>
2020-11-07 07:50:46 +0100

    Dodat prikaz osnovnih informacija za sve tipove prostorija i ubačeni prozori za dodatne informacije o sobama i salama

--- GraphicEditor/View/Building1/Building1FirstFloorPlan.xaml.cs
+++ GraphicEditor/View/Building1/Building1FirstFloorPlan.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -51,6 +52,65 @@ namespace GraphicEditor.View.Building1
             {
                 mainWindow.comboBoxHospital1.SelectedIndex = 0;
             }
+        }
+
+        private void PatientRoom_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PopupPatientRoom.Placement = PlacementMode.MousePoint;
+            PopupPatientRoom.IsOpen = true;
+        }
+
+        private void PatientRoom_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PopupPatientRoom.Visibility = Visibility.Collapsed;
+            PopupPatientRoom.IsOpen = false;
+        }
+
+        private void ExaminationRoom_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PopupExaminationRoom.Placement = PlacementMode.MousePoint;
+            PopupExaminationRoom.IsOpen = true;
+        }
+
+        private void ExaminationRoom_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PopupExaminationRoom.Visibility = Visibility.Collapsed;
+            PopupExaminationRoom.IsOpen = false;
+        }
+
+        private void OperatingRoom_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PopupOperatingRoom.Placement = PlacementMode.MousePoint;
+            PopupOperatingRoom.IsOpen = true;
+        }
+
+        private void OperatingRoom_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PopupOperatingRoom.Visibility = Visibility.Collapsed;
+            PopupOperatingRoom.IsOpen = false;
+        }
+
+        private void AuxiliaryRoom_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PopupAuxiliaryRoom.Placement = PlacementMode.MousePoint;
+            PopupAuxiliaryRoom.IsOpen = true;
+        }
+
+        private void AuxiliaryRoom_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PopupAuxiliaryRoom.Visibility = Visibility.Collapsed;
+            PopupAuxiliaryRoom.IsOpen = false;
+        }
+        private void AdditionalInformationPatientRoom(object sender, MouseButtonEventArgs e)
+        {
+            AdditionalInformationPatientRoom additionalInformation = new AdditionalInformationPatientRoom();
+            additionalInformation.ShowDialog();
+        }
+
+        private void AdditionalInformationOperatingRoom(object sender, MouseButtonEventArgs e)
+        {
+            AdditionalInformationOperaingRoom additionalInformation = new AdditionalInformationOperaingRoom();
+            additionalInformation.ShowDialog();
         }
     }
 }
