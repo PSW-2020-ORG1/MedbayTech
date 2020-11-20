2f4c2e070e42be59935d375a0fcf2637127f8270
Markovic Filip <markovicfilip98@gmail.com>
2020-11-20 21:48:58 +0100

    :recycle:  Refactored model added QuestionType and SurveyAnswer

---
+++ Backend/Users/Model/SurveyAnswer.cs
@@ -0,0 +1,34 @@
+﻿using Model.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Backend.Users.Model
+{
+    public class SurveyAnswer
+    {
+        public int Id { get; protected set; }
+        [ForeignKey("Survey")]
+        public int SurveyId { get; protected set; }
+        public virtual Survey Survey { get; set; }
+        public Grade Grade { get; protected set; }
+        public int SurveyQuestionId { get; protected set; }
+
+
+        public SurveyAnswer() { }
+
+        public SurveyAnswer(int id, Survey survey, Grade grade, int surveyQuestionId ) 
+        {
+            Id = id;
+            Survey = survey;
+            SurveyId = survey.Id;
+            Grade = grade;
+            SurveyQuestionId = surveyQuestionId;
+        }
+    }
+
+
+
+    
+}
