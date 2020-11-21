cb1d19d31810d77c9d6a439d57472b5144ac6db5
Markovic Filip <markovicfilip98@gmail.com>
2020-11-21 04:19:09 +0100

    :white_check_mark: Added a test for getting all active questions

---
+++ MedbayTechUnitTests/SurveyTest.cs
@@ -0,0 +1,20 @@
+using Backend.Users.Model.Enums;
+using Backend.Users.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Users;
+using Moq;
+using System.Collections.Generic;
+using WebApplicationService.GeneralService;
+
+namespace MedbayTechUnitTests
+{
+    [TestClass]
+    public class SurveyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
