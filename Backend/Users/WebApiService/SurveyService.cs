2a346074fc78dd431c073d843e1940ca4db47078
Markovic Filip <markovicfilip98@gmail.com>
2020-11-23 12:54:33 +0100

    :construction: Implemented Survey service and controller  and postSurveyDTO

---
+++ Backend/Users/WebApiService/SurveyService.cs
@@ -0,0 +1,58 @@
+﻿using Backend.Users.Model;
+using Backend.Users.Repository;
+using Backend.Users.Repository.MySqlRepository;
+using Model.Schedule;
+using Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Users.WebApiService
+{
+    public class SurveyService
+    {
+        private ISurveyQuestionRepository surveyQuestionRepository;
+        private ISurveyRepository surveyRepository; 
+
+        public SurveyService(ISurveyQuestionRepository surveyQuestionRepository, ISurveyRepository surveyRepository) {
+            this.surveyQuestionRepository = surveyQuestionRepository;
+            this.surveyRepository = surveyRepository;
+        }
+
+        public IEnumerable<SurveyQuestion> GetAllQuestions()
+        {
+            return surveyQuestionRepository.GetAll();
+        }
+
+        public IEnumerable<SurveyQuestion> GetAllActiveQuestions()
+        {
+            return surveyQuestionRepository.GetAllActiveQuestions();
+        }
+        public Survey CreateSurvey(List<SurveyAnswer> surveyAnswers, Appointment appointment)
+        {
+            
+            int surveyId = GenerateSurveyId();
+            Survey survey = new Survey();
+            survey.AppointmentId = appointment.Id;
+            survey.Appointment = appointment;
+            survey.Date = DateTime.Now;
+            survey.SurveyAnswers = surveyAnswers;
+   
+            Survey createdSurvey = surveyRepository.Create(survey);
+
+            return createdSurvey;
+        }
+
+        public int GenerateSurveyId()
+        {      
+            int id;
+            if (surveyRepository.GetAll() == null)
+            {
+                id = 1;
+                return id;
+            }
+            id = surveyRepository.GetLastId();
+            return ++id;
+        }
+    }
+}
---
+++ WebApplication/DTO/PostSurveyDTO.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Users.Model;
+using Model.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DTO
+{
+    public class PostSurveyDTO
+    {
+        public Appointment Appointment { get; set; }
+        public List<SurveyAnswer> SurveyAnswers { get; set; }
+        public PostSurveyDTO() { }
+        public PostSurveyDTO(Appointment appointment, List<SurveyAnswer> surveyAnswers)
+        {
+            Appointment = appointment;
+            SurveyAnswers = surveyAnswers;
+        }
+
+    }
+}
