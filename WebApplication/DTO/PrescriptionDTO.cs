012409349a75180f453f6837ea05fd29617f58c6
Mihailo Ivić <oliahim.ivic@gmail.com>
2020-11-27 19:06:58 +0100

    :construction: Added DTOs for prescription and report

---
+++ WebApplication/DTO/PrescriptionDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DTO
+{
+    public class PrescriptionDTO
+    {
+        public String Medication { get; set; }
+        public int HourlyIntake { get; set; }
+        public DateTime Date { get; set; }
+
+        public PrescriptionDTO(string medication, int hourlyIntake, DateTime date)
+        {
+            Medication = medication;
+            HourlyIntake = hourlyIntake;
+            Date = date;
+        }
+    }
+}
---
+++ WebApplication/DTO/ReportDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DTO
+{
+    public class ReportDTO
+    {
+        public String Doctor { get; set; }
+        public DateTime Date { get; set; }
+        public String Treatment { get; set; }
+
+        public ReportDTO(string doctor, DateTime date, string treatment)
+        {
+            Doctor = doctor;
+            Date = date;
+            Treatment = treatment;
+        }
+    }
+}
