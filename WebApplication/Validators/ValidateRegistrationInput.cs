44424ffea1391c535a47a134a41298d33ce66a86
Bojan <vujic.ra170.2017@uns.ac.rs>
2020-11-27 02:51:10 +0100

    :sparkle: Validation

---
+++ WebApplication/Validators/ValidateRegistrationInput.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Users.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebApplication.DTO;
+
+namespace WebApplication.Validators
+{
+    public class ValidateRegistrationInput
+    {
+        public static void Validate(PatientRegistrationDTO dto)
+        {
+            IsNameValid(dto.Name);
+        }
+
+        public static void IsNameValid(string name)
+        {
+            string namePattern = "^[A-ZŠĐŽČĆ][a-zšđćčžA-ZŠĐŽČĆ ]*$";
+            Regex regex = new Regex(namePattern);
+            if (name == null)
+                throw new ValidationException("Enter name");
+
+            if (regex.IsMatch(namePattern))
+                throw new ValidationException("Enter name in valid format");
+        }
+    }
+}
