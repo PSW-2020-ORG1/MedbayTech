ebbd18914152579313955f31d47b4188f315af7d
Bojan <vujic.ra170.2017@uns.ac.rs>
2020-11-27 02:49:32 +0100

    :sparkle: Implemented logic for activating account

--- Backend/Users/WebApiService/RegistrationService.cs
+++ Backend/Users/WebApiService/RegistrationService.cs
@@ -18,13 +18,13 @@ namespace Backend.Users.WebApiService
 
         public Patient Register(Patient patient)
         {
-            if(!ExistsById(patient.Id))
+            if (!ExistsById(patient.Id))
             {
                 return patientRepository.Create(patient);
             }
 
             return null;
-        } 
+        }
 
         public bool ExistsById(string id)
         {
@@ -35,11 +35,63 @@ namespace Backend.Users.WebApiService
         {
             List<Patient> patients = patientRepository.GetAll().ToList();
             bool exists = patients.Any(patient => patient.Username != null && patient.Username.Equals(username));
-            if(exists)
+            if (exists)
             {
                 return patients.FirstOrDefault(patient => patient.Username.Equals(username));
             }
             return null;
+        }
+
+        internal Patient SetImagePath(string path, string id)
+        {
+            bool exists = ExistsById(id);
+            if(exists)
+            {
+                Patient patient = GetUserById(id);
+                patient.ProfileImage = path;
+                patientRepository.Update(patient);
+                return patient;
+            }
+            return null;
+        }
+
+        public Patient ActivateAccount(string userId, string token)
+        {
+            Patient patient = CheckToken(userId, token);
+            if (patient == null) return patient;
+
+            return ChangeAccountState(patient, true);
+
+        }
+
+        private Patient CheckToken(string userId, string token)
+        {
+            if (!ExistsById(userId)) return null;
+
+            Patient _patient = GetUserById(userId);
+            string userToken = _patient.Token;
+
+            if (!token.Equals(userToken)) return null;
+
+            _patient.Confirmed = true;
+            patientRepository.Update(_patient);
+
+            return _patient;
+        }
+
+        private Patient ChangeAccountState(Patient patient, bool state)
+        {
+            patient.Confirmed = state;
+            return patientRepository.Update(patient);
+        }
+        public Patient GetFirstPatient()
+        {
+            return patientRepository.GetAll().FirstOrDefault();
+        }
+
+        public Patient GetUserById(string id)
+        {
+            return patientRepository.GetById(id);
         }
 
     }
