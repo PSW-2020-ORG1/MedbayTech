36d6b0f444687de78951e2c92aa58c242d038f5c
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2020-11-28 03:03:53 +0100

    :construction: added backend validation for reports

---
+++ WebApplication/DTO/ReportAdvancedDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DTO
+{
+    public class ReportAdvancedDTO
+    {
+        public string DoctorName { get; set; }
+        public string DoctorSurname { get;set; }
+        public string AndOr { get; set; }
+        public string Diagnosis { get; set; }
+        public string DateOfExamination { get; set; }
+        public string Allergens { get; set; }
+
+        public ReportAdvancedDTO(string doctorName, string doctorSurname, string andOr, string diagnosis, string date, string allergens)
+        {
+            this.DoctorName = doctorName;
+            this.DoctorSurname = doctorSurname;
+            this.AndOr = andOr;
+            this.Diagnosis = diagnosis;
+            this.DateOfExamination = date;
+            this.Allergens = allergens;
+        }
+    }
+}
---
+++ WebApplication/Validators/ValidateReportSearchInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.DTO;
+
+namespace WebApplication.Validators
+{
+    public class ValidateReportSearchInput : ValidateAuthInput
+    {
+        public static void Validate(ReportAdvancedDTO dto)
+        {
+            IsNameValid(dto.DoctorName);
+            IsSurnameValid(dto.DoctorSurname);
+            IsDiagnosisValid(dto.Diagnosis);
+            IsAllergenValid(dto.Allergens);
+            IsDateValid(dto.DateOfExamination);
+        }
+    }
+}
