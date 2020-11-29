c6ca818d6d2d7b8d89a63521a5c3c44762b86cf7
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2020-11-29 01:42:10 +0100

    :white_check_mark: added test for checking if prescriptions are not found

--- MedbayTechUnitTests/Examinations/PrescriptionTests.cs
+++ MedbayTechUnitTests/Examinations/PrescriptionTests.cs
@@ -15,31 +15,87 @@ using Backend.Examinations.Model.Enums;
 using Backend.Medications.Model;
 using Backend.Examinations.WebApiService;
 using WebApplication.DTO;
+using ZdravoKorporacija.Model.Users;
+using Backend.Records.Model;
+using Backend.Records.Model.Enums;
+using Model.Schedule;
 
 namespace MedbayTechUnitTests.Examinations
 {
     public class PrescriptionTests
     {
-        [Fact]
-        public void Search_advanced_prescriptions_integration()
-        {
-            throw new NotImplementedException(); 
-        }
 
         [Fact]
-        public void Search_advanced_prescription_unit()
+        public void Advanced_presciption_not_found()
         {
             var stubRepository = CreateStubRepository();
-            PrescriptionAdvancedDTO dto = new PrescriptionAdvancedDTO();
+            PrescriptionAdvancedDTO dto = CreateFalseDTO();
             PrescriptionWebService service = new PrescriptionWebService(stubRepository);
 
             List<Prescription> prescriptions = service.AdvancedSearchPrescriptions(dto);
 
-            prescriptions.ShouldNotBeNull();
+            prescriptions.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void Advanced_prescription_found()
+        {
+            var stubRepository = CreateStubRepository();
+            PrescriptionAdvancedDTO dto = CreateDTO();
+            PrescriptionWebService service = new PrescriptionWebService(stubRepository);
+
+            List<Prescription> prescriptions = service.AdvancedSearchPrescriptions(dto);
+
+            prescriptions.ShouldNotBeEmpty();
 
             
         }
 
+        public static PrescriptionAdvancedDTO CreateDTO()
+        {
+            PrescriptionAdvancedDTO dto = new PrescriptionAdvancedDTO
+            {
+                FirstParameterType = "houlyIntake",
+                FirstParameterValue = "5",
+                LogicOperators = new string[2],
+                OtherParameterTypes = new string[2],
+                OtherParameterValues = new string[2]
+            };
+
+            dto.LogicOperators[0] = "and";
+            dto.LogicOperators[1] = "or";
+
+            dto.OtherParameterValues[0] = "Brufen";
+            dto.OtherParameterValues[1] = "6";
+
+            dto.OtherParameterTypes[0] = "medication";
+            dto.OtherParameterTypes[1] = "hourlyIntake";
+
+            return dto;
+        }
+
+        public static PrescriptionAdvancedDTO CreateFalseDTO()
+        {
+            PrescriptionAdvancedDTO dto = new PrescriptionAdvancedDTO
+            {
+                FirstParameterType = "houlyIntake",
+                FirstParameterValue = "5",
+                LogicOperators = new string[2],
+                OtherParameterTypes = new string[2],
+                OtherParameterValues = new string[2]
+            };
+
+            dto.LogicOperators[0] = "and";
+            dto.LogicOperators[1] = "or";
+
+            dto.OtherParameterValues[0] = "Brufen";
+            dto.OtherParameterValues[1] = "2";
+
+            dto.OtherParameterTypes[0] = "medication";
+            dto.OtherParameterTypes[1] = "hourlyIntake";
+
+            return dto;
+        }
 
         public static IPrescriptionRepository CreateStubRepository()
         {
@@ -49,6 +105,54 @@ namespace MedbayTechUnitTests.Examinations
                 Id = 1,
                 Med = "Brufen"
 
+            };
+
+            var patient = new Patient
+            {
+                Id = "2406978890046",
+                CurrResidenceId = 1,
+                DateOfBirth = new DateTime(1978, 6, 24),
+                DateOfCreation = new DateTime(),
+                EducationLevel = EducationLevel.bachelor,
+                Email = "pera@gmail.com",
+                Gender = Gender.MALE,
+                InsurancePolicyId = "policy1",
+                Name = "Petar",
+                Surname = "Petrovic",
+                Username = "pera",
+                Password = "pera1978",
+                Phone = "065/123-4554",
+                PlaceOfBirthId = 11000,
+                Profession = "vodoinstalater",
+                ProfileImage = ".",
+                IsGuestAccount = false,
+                ChosenDoctorId = "2406978890047"
+            };
+
+            var medicalRecord = new MedicalRecord
+            {
+                Id = 1,
+                CurrHealthState = PatientCondition.HospitalTreatment,
+                BloodType = BloodType.ANeg,
+                Allergies = new List<Allergens>(),
+                Vaccines = new List<Vaccines>(),
+                IllnessHistory = new List<Diagnosis>(),
+                FamilyIllnessHistory = new List<FamilyIllnessHistory>(),
+                PatientId = "2406978890046",
+                Therapies = new List<Therapy>(),
+                Patient = patient
+            };
+
+            var examinationSurgery = new ExaminationSurgery
+            {
+                Id = 3,
+                DoctorId = "2406978890047",
+                StartTime = new DateTime(2015, 4, 23),
+                MedicalRecordId = 1,
+                Type = TypeOfAppointment.Examination,
+                Diagnoses = new List<Diagnosis>(),
+                Treatments = new List<Treatment>(),
+                MedicalRecord = medicalRecord
             };
 
             var prescription = new Prescription
@@ -61,7 +165,8 @@ namespace MedbayTechUnitTests.Examinations
                 Date = new DateTime(),
                 ExaminationSurgeryId = 3,
                 Type = TreatmentType.Prescription,
-                Medication = medication
+                Medication = medication,
+                ExaminationSurgery = examinationSurgery
                 
             };
 
@@ -69,7 +174,7 @@ namespace MedbayTechUnitTests.Examinations
             {
                 Id = 2,
                 Reserved = false,
-                HourlyIntake = 7,
+                HourlyIntake = 6,
                 MedicationId = 1,
                 AdditionalNotes = "svasta nesto",
                 Date = new DateTime(),
@@ -84,7 +189,7 @@ namespace MedbayTechUnitTests.Examinations
             prescriptions.Add(prescription);
             prescriptions.Add(prescription2);
 
-            stubRepository.Setup(p => p.GetAll()).Returns(prescriptions);
+            stubRepository.Setup(p => p.GetPrescriptionsFor("2406978890046")).Returns(prescriptions);
             
             
 
