2e1ce4bfea27359ef0f4993227923520cafa69f2
Markovic Filip <markovicfilip98@gmail.com>
2020-11-29 16:00:02 +0100

    ✨ added all DTOs from Web Application to Backend

---
+++ Backend/Users/DTO/ApprovedFeedbackDTO.cs
@@ -0,0 +1,30 @@
+﻿using SimsProjekat.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.DTO
+{
+    public class ApprovedFeedbackDTO
+    {
+        public DateTime Date { get; set; }
+        public string AdditionalNotes { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public bool AllowedForPublishing { get; set; }
+
+        public bool Anonymous { get; set; }
+        public ApprovedFeedbackDTO(DateTime date, string additionalNotes, string name, string surname, bool allowedForPublishing, bool anonymous)
+        {
+            Date = date;
+            AdditionalNotes = additionalNotes;
+            Name = name;
+            Surname = surname;
+            AllowedForPublishing = allowedForPublishing;
+            Anonymous = anonymous;
+        }
+    }
+}
