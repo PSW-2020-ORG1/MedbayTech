5685836acfccc740cc731bc9e54891a9f315592b
Stefan Aradjanin <58364787+st3fan7@users.noreply.github.com>
2020-11-30 05:13:50 +0100

    :construction: Refactoring graphic editor (#27)

--- Backend/Medications/Service/MedicationService.cs
+++ Backend/Medications/Service/MedicationService.cs
@@ -17,105 +17,120 @@ namespace Backend.Medications.Service
 {
     public class MedicationService : IMedicationService
     {
-        public IMedicationRepository _medicationRepository;
+        public IMedicationRepository medicationRepository;
         public NotificationService notificationService;
 
-        private MySqlContext _context;
-
-        public MedicationService ( IMedicationRepository medicationRepository )
+        public List<Medication> GetAllMedicationsByNameOrId(string query)
         {
+             if (Int32.TryParse(query, out int id))
+                return _context.Medications.Where(med => med.Id == id).ToList();
+            return _context.Medications.Where(med => med.Med.ToLower().Equals(query.ToLower())).ToList();
+
+        }
+
+        public IValidationMedicationRepository validationMedicationRepository;
+        public IMedicationRepository _medicationRepository;
+
+
+        private MySqlContext _context;
+        public MedicationService(MySqlContext context)
+        {
+            _context = context;
+        }
+
+        public MedicationService(IMedicationRepository medicationRepository, IValidationMedicationRepository validationMedicationRepository)
+        {
+            this.validationMedicationRepository = validationMedicationRepository;
+            this.medicationRepository = medicationRepository;
+        }
+
+        public MedicationService(IMedicationRepository medicationRepository)
+        {
+            this.validationMedicationRepository = validationMedicationRepository;
+            this.medicationRepository = medicationRepository;
             this._medicationRepository = medicationRepository;
         }
 
-        public List<Medication> GetAllMedicationsByNameOrId ( string nameOfMedication )
-        {
-            List<Medication> medications = new List<Medication>();
-            if ( Int32.TryParse(nameOfMedication, out int id) )
-            {
-                medications = _medicationRepository.GetAll().ToList().Where(p => p.Id == id).ToList();
-                if ( medications.Count != 0 ) return medications;
-            }
-            medications = _medicationRepository.GetAll().ToList().Where(p => p.Med.ToLower().Contains(nameOfMedication.ToLower())).ToList();
-            return medications;
-        }
-
-        public Medication RejectMedication ( Medication medication )
+        public Medication RejectMedication(Medication medication)
         {
             medication.Status = MedStatus.Approved;
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
 
-        public Medication ApproveMedication ( Medication medication )
+        public Medication ApproveMedication(Medication medication)
         {
             medication.Status = MedStatus.Rejected;
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
 
-        public Medication CreateMedication ( Medication medication )
+        public Medication CreateMedication(Medication medication)
         {
             Medication fullMedication = medication;
-            _medicationRepository.Create(medication);
+            medicationRepository.Create(medication);
             notificationService.MedForValidationNotification(medication);
             return medication;
         }
 
-        public Medication UpdateMedication ( Medication medication ) =>
-            _medicationRepository.Update(medication);
+        public Medication UpdateMedication(Medication medication) =>
+            medicationRepository.Update(medication);
 
-        public bool DeleteMedication ( Medication medication ) =>
-            _medicationRepository.Delete(medication);
+        public bool DeleteMedication(Medication medication) =>
+            medicationRepository.Delete(medication);
 
-        public Medication GetMedication ( int id ) =>
-            _medicationRepository.GetObject(id);
+        public Medication GetMedication(int id) =>
+            medicationRepository.GetObject(id);
 
-        public IEnumerable<Medication> GetAllOnValidationFor ( Doctor doctor )
+        public IEnumerable<Medication> GetAllOnValidationFor(Doctor doctor)
         {
-            List<Medication> allOnValidation = ( List<Medication> ) _medicationRepository.GetAllOnValidation().ToList();
+            List<Medication> allOnValidation = (List<Medication>)medicationRepository.GetAllOnValidation().ToList();
             List<Medication> validations = new List<Medication>();
-            foreach ( Medication medication in allOnValidation )
+            foreach (Medication medication in allOnValidation)
             {
                 Specialization specialization = medication.MedicationCategory.Specialization;
-                if ( doctor.IsMySpecialization(specialization) && medication.IsOnValidation() )
+                if (doctor.IsMySpecialization(specialization) && medication.IsOnValidation())
 
                     validations.Add((medication));
             }
             return validations;
         }
 
-        public IEnumerable<Medication> GetAllOnValidation ( ) =>
-            _medicationRepository.GetAllOnValidation();
+        public IEnumerable<Medication> GetAllOnValidation() =>
+            medicationRepository.GetAllOnValidation();
 
-        public IEnumerable<Medication> GetAll ( ) =>
-            _medicationRepository.GetAll();
+        public IEnumerable<Medication> GetAll() =>
+            medicationRepository.GetAll();
 
-        public IEnumerable<Medication> GetAllRejected ( ) =>
-            _medicationRepository.GetAllRejected();
+        public IEnumerable<Medication> GetAllRejected() =>
+            medicationRepository.GetAllRejected();
 
-        public IEnumerable<Medication> GetAllApproved ( ) =>
-            _medicationRepository.GetAllApproved();
+        public IEnumerable<Medication> GetAllApproved() =>
+            medicationRepository.GetAllApproved();
 
-        public Medication AddAmount ( Medication medication, int amount )
+        public Medication AddAmount(Medication medication, int amount)
         {
             medication.Quantity += amount;
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
 
-        public Medication UpdateSideEffects ( Medication medication, SideEffect sideEffects )
+        public Medication UpdateSideEffects(Medication medication, SideEffect sideEffects)
         {
             medication.SideEffects.Add(sideEffects);
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
 
-        public Medication UpdateDosageOfIngredients ( Medication medication, DosageOfIngredient dosageOfIngredient )
+        public Medication UpdateDosageOfIngredients(Medication medication, DosageOfIngredient dosageOfIngredient)
         {
             medication.MedicationContent.Add(dosageOfIngredient);
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
 
-        public Medication UpdateAllergens ( Medication medication, Allergens allergens )
+        public Medication UpdateAllergens(Medication medication, Allergens allergens)
         {
             medication.Allergens.Add(allergens);
-            return _medicationRepository.Update(medication);
+            return medicationRepository.Update(medication);
         }
+
+
+
     }
 }
--- Backend/Rooms/Service/HospitalEquipmentService.cs
+++ Backend/Rooms/Service/HospitalEquipmentService.cs
@@ -28,8 +28,14 @@ namespace Service.RoomService
             this._hospitalEquipmentRepository = hospitalEquipmentRepository;
         }
 
+        
+
+
+
         public List<HospitalEquipment> GetHospitalEquipmentsByNameOrId ( string textBoxSearch )
         {
+
+            /*
             List<HospitalEquipment> hospitalEquipment = new List<HospitalEquipment>();
             if ( Int32.TryParse(textBoxSearch, out int id) )
             {
@@ -37,8 +43,13 @@ namespace Service.RoomService
                 if ( hospitalEquipment.Count != 0 ) return hospitalEquipment;
             }
             hospitalEquipment = _hospitalEquipmentRepository.GetAll().ToList().Where(p => p.EquipmentType.Name.ToLower().Trim().Contains(textBoxSearch.ToLower())).ToList();
-
+            
             return hospitalEquipment;
+            */
+
+            if (Int32.TryParse(textBoxSearch, out int id))
+                return _context.HospitalEquipment.Where(med => med.Id == id).ToList();
+            return _context.HospitalEquipment.Where(med => med.EquipmentType.Name.ToLower().Equals(textBoxSearch.ToLower())).ToList();
         }
 
         public HospitalEquipment AddEquipment ( HospitalEquipment equipment ) => _hospitalEquipmentRepository.Create(equipment);
