971a06d1de15b55e20d1d4d9efc8a9591b962a77
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2020-12-07 12:42:41 +0100

    :white_check_mark: Added unit tests for blocking patient

---
+++ UnitTests/Users/PatientBlockingTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.Users
+{
+    public class PatientBlockingTests
+    {
+
+        [Fact]
+        public void Block_patient_by_id()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Dont_block_patient_by_id()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
