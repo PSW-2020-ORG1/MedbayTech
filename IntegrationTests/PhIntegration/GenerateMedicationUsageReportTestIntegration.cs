3163c2b64e9609315075d430366d084d9b79f36a
Kris98Wolf <kristian98farkas@gmail.com>
2020-12-19 19:46:02 +0100

    :white_check_mark: Added integration tests for usage report

---
+++ IntegrationTests/PhIntegration/GenerateMedicationUsageReportTestIntegration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using PharmacyIntegration;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+using Backend.Utils;
+
+namespace IntegrationTests.PhIntegration
+{
+    public class GenerateMedicationUsageReportTestIntegration : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public GenerateMedicationUsageReportTestIntegration(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void Post_generate_medication_usage_report()
+        {
+            HttpClient client = _factory.CreateClient();
+            var period = CreatePeriod();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(period), System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("/api/MedicationUsageReport", content);
+            response.StatusCode.ShouldBeEquivalentTo(HttpStatusCode.OK);
+        }
+
+        private Period CreatePeriod()
+        {
+            Period period = new Period
+            {
+                StartTime = new DateTime(2020, 8, 10), 
+                EndTime = new DateTime(2020, 9, 15),
+            };
+            return period;
+        }
+
+    }
+}
