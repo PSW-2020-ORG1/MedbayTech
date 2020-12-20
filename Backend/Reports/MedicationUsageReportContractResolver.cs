9f5aff9093da7b4937bf3b7a78b2e19508086a90
L4v <ivosevic.jovan@uns.ac.rs>
2020-12-20 21:05:16 +0100

    :ambulance: JsonProperty Ignore hotfix
    * :ambulance: JsonProperty Ignore hotfix
    
    * :card_file_box: Added more data to dataseeder.
    
    Co-authored-by: Jelena Vlajkov <57070700+jelena-vlajkov@users.noreply.github.com>
    Co-authored-by: OakenKnight <alexignjat1998@gmail.com>

---
+++ Backend/Reports/MedicationUsageReportContractResolver.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Backend.Reports
+{
+    public class MedicationUsageReportContractResolver : DefaultContractResolver
+    {
+        private readonly Dictionary<Type, HashSet<string>> _ignores;
+
+        public MedicationUsageReportContractResolver()
+        {
+            _ignores = new Dictionary<Type, HashSet<string>>();
+        }
+
+        public void IgnoreProperty(Type type, params string[] jsonPropertyNames)
+        {
+            if(!_ignores.ContainsKey(type))
+            {
+                _ignores[type] = new HashSet<string>();
+            }
+
+            foreach(var prop in jsonPropertyNames)
+            {
+                _ignores[type].Add(prop);
+            }
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            if(IsIgnored(property.DeclaringType, property.PropertyName))
+            {
+                property.ShouldSerialize = i => false;
+                property.Ignored = true;
+            }
+            return property;
+        }
+
+        private bool IsIgnored(Type type, string jsonPropertyName)
+        {
+            if(!_ignores.ContainsKey(type))
+            {
+                return false;
+            }
+            return _ignores[type].Contains(jsonPropertyName);
+        }
+    }
+}
