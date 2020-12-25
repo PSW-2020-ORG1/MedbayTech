4fd57707ecb76453702473d18a22a876e8ca57b4
Marko <marko.ppekez25@gmail.com>
2020-12-25 18:49:20 +0100

    :sparkles: Created urgent procurement for medication.

---
+++ Backend/Pharmacies/Model/UrgentMedicationProcurement.cs
@@ -0,0 +1,36 @@
+﻿using Backend.General.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Backend.Pharmacies.Model
+{
+    public class UrgentMedicationProcurement : IIdentifiable<int>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string MedicationName { get; set; }
+        public string MedicationDosage { get; set; }
+        public int MedicationQuantity { get; set; }
+
+        public UrgentMedicationProcurement(string medicationName, string medicationDosage, int medicationQuantity)
+        {
+            MedicationName = medicationName;
+            MedicationDosage = medicationDosage;
+            MedicationQuantity = medicationQuantity;
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+    }
+}
