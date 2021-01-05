2691b6efc4ee607bf22b1a9320e4455b671ec59d
Marko <marko.ppekez25@gmail.com>
2021-01-05 23:20:04 +0100

    :bug: Inherited MySqlRepository into repositories.

--- Backend/Pharmacies/Repository/MySqlRepository/UrgentMedicationProcurementSqlRepository.cs
+++ Backend/Pharmacies/Repository/MySqlRepository/UrgentMedicationProcurementSqlRepository.cs
@@ -1,51 +1,18 @@
 ﻿using Backend.Pharmacies.Model;
 using Model;
 using PharmacyIntegration.Repository;
+using Repository;
 using System.Collections.Generic;
 using System.Linq;
 
 
 namespace Backend.Pharmacies.Repository.MySqlRepository
 {
-    public class UrgentMedicationProcurementSqlRepository : IUrgentMedicationProcurementRepository //MySqlrepository<UrgentMedicationProcurement, int>
+    public class UrgentMedicationProcurementSqlRepository : MySqlrepository<UrgentMedicationProcurement, int>, IUrgentMedicationProcurementRepository 
     {
-
-        private MedbayTechDbContext _context;
-
-        public UrgentMedicationProcurementSqlRepository(MedbayTechDbContext context)
+        
+        public UrgentMedicationProcurementSqlRepository(MedbayTechDbContext context):base(context)
         {
-            _context = context;
-        }
-        public UrgentMedicationProcurement Create(UrgentMedicationProcurement entity)
-        {
-            if (ExistsInSystem(entity.Id))
-            {
-                return null;
-            }
-            _context.UrgentMedicationProcurements.Add(entity);
-            _context.SaveChanges();
-            return entity;
-        }
-
-        public bool Delete(UrgentMedicationProcurement entity)
-        {
-            _context.UrgentMedicationProcurements.Remove(entity);
-            _context.SaveChanges();
-            return true;
-        }
-
-        public bool ExistsInSystem(int id) => GetObject(id) != null;
-
-        public List<UrgentMedicationProcurement> GetAll() => _context.UrgentMedicationProcurements.ToList();
-
-        public UrgentMedicationProcurement GetObject(int id) =>
-            _context.UrgentMedicationProcurements.ToList().Find(pn => pn.Id.Equals(id));
-
-        public UrgentMedicationProcurement Update(UrgentMedicationProcurement entity)
-        {
-            _context.UrgentMedicationProcurements.Update(entity);
-            _context.SaveChanges();
-            return entity;
         }
     }
 }
