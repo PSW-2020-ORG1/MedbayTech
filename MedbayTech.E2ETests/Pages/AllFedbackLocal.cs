5b104cefe7534b1c21ac8a9796492e874f516a84
OakenKnight <alexignjat1998@gmail.com>
2021-01-10 01:27:06 +0100

    :white_check_mark: Fixed selenium e2e feedback test for local deploy

---
+++ MedbayTech.E2ETests/Pages/AllFedbackLocal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumEndToEnd.Pages
+{
+    public class AllFedbackLocal
+    {
+        private IWebDriver driver;
+        public const string URI = "http://localhost:4200/#/allFeedback";
+
+        private IWebElement CountFeedback => driver.FindElement(By.Name("all_feedback_len"));
+        private IWebElement TitleFeedbacks => driver.FindElement(By.Name("feedbacks"));
+        private IWebElement BlockPatientsButton => driver.FindElement(By.Name("block-malicious-users"));
+        public AllFedbackLocal(IWebDriver webdriver)
+        {
+            driver = webdriver;
+        }
+
+        public void EnsurePageIsDisplayed()
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 20));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return GetFeedbackCount() > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public bool MaliciousPatientsLinkElementDisplayed()
+        {
+            return BlockPatientsButton.Displayed;
+        }
+
+        public void ClickMaliciousPatientsLink()
+        {
+            BlockPatientsButton.Click();
+        }
+
+        public int GetFeedbackCount()
+        {
+            string[] str = CountFeedback.Text.Split(':');
+            string number = str[1].Trim().Split(' ')[0];
+            return int.Parse(number);
+        }
+
+
+        public bool TitleDisplayed()
+        {
+            return TitleFeedbacks.Displayed;
+        }
+
+        public void Navigate() => driver.Navigate().GoToUrl(URI);
+    }
+}
---
+++ MedbayTech.E2ETests/Pages/CreateFeedbackLocal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumEndToEnd.Pages
+{
+    public class CreateFeedbackLocal
+    {
+        private IWebDriver _webDriver { get; }
+        public const string URI = "http://localhost:53843/#/createFeedback";
+
+        public CreateFeedbackLocal(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        IWebElement txtFeedback => _webDriver.FindElement(By.Name("additionalNotes"));
+
+        IWebElement radioAllowed => _webDriver.FindElement(By.Id("allowedNo"));
+
+        IWebElement radioAnonymous => _webDriver.FindElement(By.Id("anonymousNo"));
+
+        private IWebElement submitButton => _webDriver.FindElement(By.XPath("//button[@value='submit']"));
+
+
+        public void SelectAllowed() => radioAllowed.Click();
+        
+        public void SelectAnonymous() => radioAnonymous.Click();
+
+        public void AddFeedback(string feedback) => txtFeedback.SendKeys(feedback);
+
+        public void SubmitFeedback() => submitButton.Submit();
+
+        public bool FeedbackTextboxDisplayed()
+        {
+            return txtFeedback.Displayed;
+        }
+
+        public bool RadioButtonAllowedDisplayed()
+        {
+            return radioAllowed.Displayed;
+        }
+        public bool RadioButtonAnonymousDisplayed()
+        {
+            return radioAnonymous.Displayed;
+        }
+        public bool SubmitButtonDisplayed()
+        {
+            return submitButton.Displayed;
+        }
+
+        public void WaitForFormSubmit()
+        {
+            var wait = new WebDriverWait(_webDriver, new TimeSpan(0, 0, 40));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.AlertIsPresent());
+        }
+        public void ResolveAlertDialog()
+            => _webDriver.SwitchTo().Alert().Accept();
+
+        public void Navigate() => _webDriver.Navigate().GoToUrl(URI);
+
+    }
+}
