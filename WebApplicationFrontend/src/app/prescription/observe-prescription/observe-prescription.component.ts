5eb712bc9d0894f15ca1039569e80d0301d0322f
ognjen98 <58110517+ognjen98@users.noreply.github.com>
2021-01-18 18:26:48 +0100

    :sparkles: Changes to feature observe prescriptions (#108)
    * :iphone: Started designing frontend for observing prescriptions
    
    * :bug: Fixed seeding symptoms and diagnosis
    
    * :construction: Worked on getting reports for appointments
    
    * :white_check_mark: Added unit and integration tests for getting report for appointment
    
    * :construction: Implemented frontend and backend for getting report for appointment
    
    * :sparkles: Implemented observe prescriptions for appointment
    
    * :construction: Improved PDF creation
    
    * :white_check_mark: Changed unit test for getting blockable patients
    
    Co-authored-by: jelena-vlajkov <vlajkovj31@gmail.com>
    Co-authored-by: Markovic Filip <markovicfilip98@gmail.com>

--- WebApplicationFrontend/src/app/prescription/observe-prescription/observe-prescription.component.ts
+++ WebApplicationFrontend/src/app/prescription/observe-prescription/observe-prescription.component.ts
@@ -8,6 +8,10 @@ import { AppointmentReport } from 'src/app/model/appointmentReport';
 import { ReportService } from 'src/app/service/report/report.service';
 import { AppointmentPrescription } from 'src/app/model/appointmentPrescription';
 import { PrescriptionService } from 'src/app/service/prescription/prescription.service';
+import pdfMake from "pdfmake/build/pdfmake";  
+import pdfFonts from "pdfmake/build/vfs_fonts";  
+import { DatePipe } from '@angular/common';
+pdfMake.vfs = pdfFonts.pdfMake.vfs; 
 
 @Injectable({
   providedIn: 'root'
@@ -43,8 +47,8 @@ export class ObservePrescriptionComponentDialog implements OnInit{
 
   generatePdf(data) {
     html2canvas(data, { allowTaint: true }).then(canvas => {
-     let HTML_Width = canvas.width*3/4;
-     let HTML_Height = canvas.height*3/4;
+     let HTML_Width = canvas.width;
+     let HTML_Height = canvas.height;
      let top_left_margin = 15;
      let PDF_Width = HTML_Width + (top_left_margin * 2);
      let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
@@ -62,4 +66,33 @@ export class ObservePrescriptionComponentDialog implements OnInit{
       pdf.save("prescriptions.pdf");
    });
  }
+
+ openPDF(){
+    var datepipe = new DatePipe('en-US');
+    let docDefinition = {
+      content: [
+      {
+        text: "PRESCRIPTIONS",
+        fontSize: 16,
+        alignment: 'center',
+        color: '#047886',
+        marginBottom: 15
+      },
+      {
+        table: {
+          headerRows: 1,
+          bold: true,
+          widths: ['*', 'auto', 'auto', 'auto'],
+          body: [
+            [{text:'Medication',bold: true}, {text:'Hourly intake',bold:true}, {text: 'Take medication from',bold:true}, {text:'Take medication to',bold:true}],
+            ...this.prescriptions.map(p => ([p.medication, p.hourlyIntake, datepipe.transform(p.validFrom.toLocaleString(),'dd.MM.yyyy'), datepipe.transform(p.validTo.toLocaleString(),'dd.MM.yyyy')])),
+            
+          ]
+        }
+      }
+      ]
+    }
+    pdfMake.createPdf(docDefinition).open();     
+   
+  }
 }
