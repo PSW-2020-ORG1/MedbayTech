59c28d28b23f1e598186158ece036aa7810ccad4
Markovic Filip <markovicfilip98@gmail.com>
2021-01-22 17:47:01 +0100

    :sparkles: Finished implementing observe reports for appointments

--- WebApplicationFrontend/src/app/reports/reports-dialog/reports-dialog.component.ts
+++ WebApplicationFrontend/src/app/reports/reports-dialog/reports-dialog.component.ts
@@ -1,8 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { AppointmentReport } from 'src/app/model/appointmentReport';
+import pdfMake from "pdfmake/build/pdfmake";
 
 @Component({
   selector: 'app-reports-dialog',
@@ -20,25 +22,47 @@ export class ReportsDialogComponent implements OnInit {
     {
       this.report = this.data.reportAppointment;
     }
-  generatePdf(data) {
-    html2canvas(data, { allowTaint: true }).then(canvas => {
-     let HTML_Width = canvas.width;
-     let HTML_Height = canvas.height;
-     let top_left_margin = 15;
-     let PDF_Width = HTML_Width + (top_left_margin * 2);
-     let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-     let canvas_image_width = HTML_Width;
-     let canvas_image_height = HTML_Height;
-     let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-     canvas.getContext('2d');
-     let imgData = canvas.toDataURL("image/jpeg", 1.0);
-     let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-     pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-     for (let i = 1; i <= totalPDFPages; i++) {
-       pdf.addPage([PDF_Width, PDF_Height], 'p');
-       pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
-     }
-      pdf.save("report.pdf");
-   });
- }
+  
+  openPDF(){
+    var datepipe = new DatePipe('en-US');
+    let docDefinition = {
+      content: [
+      {
+        text: "REPORTS",
+        fontSize: 16,
+        alignment: 'center',
+        color: '#047886',
+        marginBottom: 15
+      },
+      {
+        text: this.report.type,
+        fontSize: 14,
+        alignment: 'center'
+      },
+      {
+        table: {
+          headerRows: 1,
+          bold: true,
+          widths: ['*', 'auto'],
+          body: [
+            [{text:'Diagnosis',bold: true}, {text:'Symptoms',bold:true}],
+            ...this.report.diagnosis.map(p => ([p.name, p.symptoms])),
+            
+          ]
+        }
+      },
+      {
+        text: "Doctor:" + " " +this.report.doctorName  + " " +this.report.doctorSurname,
+        fontSize: 12
+      },
+      {
+        text: "Date:" + " " + datepipe.transform(this.report.startTime.toLocaleString(),'dd.MM.yyyy'),
+        fontSize: 12,
+      },
+      ]
+    }
+    pdfMake.createPdf(docDefinition).open();     
+  
+  }
 }
+
