7a2f47614d1003dc75d8bc1851400bb53454b0a2
jelena-vlajkov <vlajkovj31@gmail.com>
2021-01-24 19:20:58 +0100

    :whale: Dockerfile for pharmacies fixed

--- MedbayTech.PharmacyIntegrationTests/PhIntegration/GenerateMedicationUsageReportTestIntegration.cs
+++ MedbayTech.PharmacyIntegrationTests/PhIntegration/GenerateMedicationUsageReportTestIntegration.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using Xunit;
-using MedbayTech.Medication;
+using MedbayTech.Medications;
 
 namespace MedbayTech.PharmacyIntegrationTests.PhIntegration
 {
